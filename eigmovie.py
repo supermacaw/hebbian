0321a5adac57e6f1183b318c55e69e3a516a2932
Datascience Course Student <datascience@datascience-amd64.defaultdomain>
2014-10-20 09:30:05 -0700

    eigenfaces implemented.. need to look at it as multiple neurons output

--- eigmovie.py
+++ eigmovie.py
@@ -23,7 +23,8 @@ def eigmovie(mu,e,lim):
     for i in range(n):
       h.set_data((mu+x[i]*e).T)
       plt.draw()
-      time.sleep(0.1)
+      #time.sleep(0.1)
+    plt.show()
 
 faces = np.load('faces2.npz')['faces2']
 faceaggregate = np.zeros((60,64))
@@ -31,34 +32,39 @@ for i in range(48):
   face = np.reshape(faces[:,i], (60,64))
   faceaggregate += face
 avgface = faceaggregate / float(48)
-print avgface.shape
 avgfacereshaped = np.reshape(avgface,(3840))
 newdata = np.zeros((3840,48))
 
 for i in range(48):
   newdata[:,i] = faces[:,i] - avgfacereshaped
 
-print newdata
-
-w1=np.random.randn(3840,1);   
-w2=np.random.randn(3840,1)     
-num_trials=200;
+w1=np.random.randn(3840,);   
+w2=np.random.randn(3840,)     
+num_trials=100;
 #eta=0.1/K;
-eta = 1
+print w1, w2
+eta = 0.01
 D = newdata
-dw1 = np.zeros((3840,1))
-dw2 = np.zeros((3840,1))
+
+dw1 = np.zeros((3840,))
+dw2 = np.zeros((3840,))
+outputs1 = np.zeros((3840,48))
+outputs2 = np.zeros((3840,48))
 for t in range(num_trials):
   # compute neuron output for all data (can be done as one line)
-  outputs1 = np.dot(D, w1)
-  outputs2 = np.dot(D, w2)
-  for i in range(len(outputs1)):
-    dw1 += np.dot(outputs1[i],D) - outputs1[i]**2 * w1
-    dw2 += outputs2[i] * (D - outputs2[i]* w2 - outputs1[i]*w1)
-  dw1[0] = dw1[0]/float(len(outputs1))
-  dw1[1] = dw1[1]/float(len(outputs1))
-  dw2[0] = dw2[0]/float(len(outputs2))
-  dw2[1] = dw2[1]/float(len(outputs2))
-  print dw1
+  for i in range(48):
+    #print w1.shape, D[:,i].shape, outputs1.shape
+    outputs1[:,i] = w1*D[:,i]
+    print outputs1[i]
+    # print w1, D[:,i]
+    #print w1, w2
+    outputs2[:,i] = w2*D[:,i]
+  for i in range(48):
+    dw1 += eta * outputs1[:,i]*(D[:,i] - outputs1[:,i] * w1)/float(len(outputs1))
+    dw2 += eta * outputs2[:,i] * (D[:,i]- outputs2[:,i]* w2 - outputs1[:,i]*w1)/float(len(outputs2))
+  print dw1, dw2
   w1 = w1 + dw1 * eta
   w2 = w2 + dw2 * eta
+
+eigmovie(avgface, np.reshape(w1,(60,64)), 10)
+eigmovie(avgface, np.reshape(w2,(60,64)), 10)
